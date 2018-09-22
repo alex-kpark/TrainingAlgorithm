f1fb83f905ea12e35b0347ea016e5fdcd52a703a
Alex Kyungho Park <30945486+konkuest@users.noreply.github.com>
2018-09-22 17:59:09 +0900

    Initial commit

---
+++ TrainingCLang/RandNum.cpp
@@ -0,0 +1,16 @@
+﻿#include <Turboc.h>
+
+int main()
+{
+	int i;
+	
+	printf("정수를 입력하세요 :");
+	scanf("%d", &i);
+	if (i % 2 == 0) {
+		printf("%d는 짝수입니다ㅎㅎ\n", i);
+	}
+	else {
+		printf("%d는 홀수입니다ㅠㅠ\n", i);
+	}
+	return i;
+}
